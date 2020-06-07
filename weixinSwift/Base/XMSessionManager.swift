32170f3f32a1b167a279bb3b64339604e5a9658d
zhangjie <137548913@qq.com>
2020-06-07 17:23:48 +0800

    init

---
+++ weixinSwift/Base/XMSessionManager.swift
@@ -0,0 +1,14 @@
+//
+//  AFHTTPSessionManager.swift
+//  weixinSwift
+//
+//  Created by 张杰 on 2020/4/20.
+//  Copyright © 2020 张杰. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class AFHTTPSessionManager: AFHTTPSessionManager {
+
+}
---
+++ weixinSwift/Home/view/MKCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MKCollectionViewCell.swift
+//  weixinSwift
+//
+//  Created by 张杰 on 2020/4/14.
+//  Copyright © 2020 张杰. All rights reserved.
+//
+
+import UIKit
+
+class MKCollectionViewCell: UICollectionViewCell {
+    
+}
